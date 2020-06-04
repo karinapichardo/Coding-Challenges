5584ddc8d9b6ca5251018e0fe74606040ffa3e70
Karina Pichardo <pichardokarina21@yahoo.com>
2020-06-04 18:54:43 -0400

    Update methods.js

--- methods.js
+++ methods.js
@@ -12,10 +12,10 @@ function firstLast(arr) {
 }
 
 // testing function
-console.log(firstLast([5, 10, 15, 20, 25]))//, [5, 25])
-console.log(firstLast(["JavaScript", 13, null, false, true])) //, ["edabit", true])
-console.log(firstLast([undefined, 4, "6", "Python", null])) //, [undefined, null])
-console.log(firstLast(["Java", "is", "really", "cool"]))//, ["hello", "com"])
-console.log(firstLast([3, 2, 1]))//, [3, 1])
-console.log(firstLast(["one", "two"]))//, ["one", "two"])
-console.log(firstLast([false, false, true, false, false, true, false]))//, [false, false])
+console.log(firstLast([5, 10, 15, 20, 25])) // --> [5, 25]
+console.log(firstLast(["JavaScript", 13, null, false, true])) // --> ["edabit", true]
+console.log(firstLast([undefined, 4, "6", "Python", null])) // --> [undefined, null]
+console.log(firstLast(["Java", "is", "really", "cool"])) // --> ["hello", "com"]
+console.log(firstLast([3, 2, 1])) // --> [3, 1]
+console.log(firstLast(["one", "two"])) // --> ["one", "two"]
+console.log(firstLast([false, false, true, false, false, true, false])) // --> [false, false]
