4de3e9f85d6856122209d38160e5d8bbbe00391c
karinapichardo <pichardokarina21@yahoo.com>
2020-06-08 18:38:47 -0400

    first commit

---
+++ numberToDashes.js
@@ -0,0 +1,15 @@
+/* 
+Create a function that takes a number (from 1 - 60) 
+and returns a corresponding string of hyphens.
+*/
+
+function Go(num) {
+	var hyphenString = "-";
+	return hyphenString.repeat(num);
+}
+
+// testing function
+console.log(Go(1)) // --> "-"
+console.log(Go(5)) // --> "-----"
+console.log(Go(3)) // --> "---"
+console.log(Go(12)) // --> "------------"
