2904a8280ed1d044b42297b7857d0cd67458ff78
karinapichardo <pichardokarina21@yahoo.com>
2020-06-11 22:34:40 -0400

    first commit

---
+++ addStrToEach.js
@@ -0,0 +1,10 @@
+// create a function that adds a string ending to each member in an array.
+
+function addEnding(arr, ending) {
+	return arr.map(x => x.concat(ending));
+}
+
+// testing function
+console.log(addEnding(["clever", "meek", "hurried", "nice"], "ly")) // --> ["cleverly", "meekly", "hurriedly", "nicely"]
+console.log(addEnding(["new", "pander", "scoop"], "er")) // --> ["newer", "panderer", "scooper"]
+console.log(addEnding(["bend", "sharpen", "mean"], "ing")) // --> ["bending", "sharpening", "meaning"]
