634e80f2ee172035d258f8e55d11954cf1a6593c
karinapichardo <pichardokarina21@yahoo.com>
2020-06-17 15:37:32 -0400

    first commit

---
+++ numToLetter.js
@@ -0,0 +1,15 @@
+/* 
+Write a function that takes an integer and 
+returns a string with the given number of "a"s in Edabit.
+*/
+
+function howManyTimes(num) {
+	var repeatedA = 'a';
+	return 'Ed' + repeatedA.repeat(num) + 'bit';
+}
+
+
+// testing function
+console.log(howManyTimes(5)) // --> "Edaaaaabit"
+console.log(howManyTimes(0)) // --> "Edbit"
+console.log(howManyTimes(12)) // --> "Edaaaaaaaaaaaabit"
