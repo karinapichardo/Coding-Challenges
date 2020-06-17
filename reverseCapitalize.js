f412da3d4285b6b459034cf3765138621b4bd284
karinapichardo <pichardokarina21@yahoo.com>
2020-06-17 14:20:10 -0400

    first commit

---
+++ reverseCapitalize.js
@@ -0,0 +1,11 @@
+/* Create a function that takes a string of lowercase characters and returns that string reversed and in upper case. */
+
+function reverseCapitalize(str) {
+	return str.split('').reverse().join('').toUpperCase();
+}
+
+// testing function
+console.log(reverseCapitalize("abc")) // --> "CBA"
+console.log(reverseCapitalize("hellothere")) // --> "EREHTOLLEH"
+console.log(reverseCapitalize("input")) // --> "TUPNI"
+console.log(reverseCapitalize("austin")) // --> "NITSUA"
