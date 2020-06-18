b3719a11a279df12094f8791b31f322900dcfc15
karinapichardo <pichardokarina21@yahoo.com>
2020-06-18 11:26:06 -0400

    first commit

---
+++ typeofBooleans.js
@@ -0,0 +1,13 @@
+// Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given.
+
+function reverse(bool) {
+	return (typeof bool === 'boolean') ? !bool : 'boolean expected';
+}
+
+// testing function
+console.log(reverse(true)) // --> false
+console.log(reverse(false)) // --> true
+console.log(reverse(0)) // --> "boolean expected"
+console.log(reverse(null)) // --> "boolean expected"
+console.log(reverse(undefined)) // --> "boolean expected"
+console.log(reverse({})) // --> "boolean expected"
