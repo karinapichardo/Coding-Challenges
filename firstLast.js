33b0dc1b4198072c39e6f0198420af5f3c4f646a
karinapichardo <pichardokarina21@yahoo.com>
2020-06-26 22:59:28 -0400

    first commit

---
+++ firstLast.js
@@ -0,0 +1,15 @@
+// Create a function that removes the first and last characters from a string.
+
+function removeFirstLast(str) {
+	if (str.length <= 2) {
+		return str;
+	} else {
+		return str.substring(1, str.length - 1);
+	}
+}
+
+// testing function
+console.log(removeFirstLast("Aaustinn")) // --> "austin"
+console.log(removeFirstLast("maybe")) // --> "ayb"
+console.log(removeFirstLast("benefit")) // --> "enefi"
+console.log(removeFirstLast("a")) // --> "a"
