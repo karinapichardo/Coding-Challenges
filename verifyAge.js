d076ef15bbc342efb32c6b0f1e8b5da2a70d5a7b
karinapichardo <pichardokarina21@yahoo.com>
2020-07-25 11:23:29 -0400

    first commit

---
+++ verifyAge.js
@@ -0,0 +1,20 @@
+/* Write a function that checks whether a person can watch an MA15+ rated movie. 
+One of the following two conditions is required for admittance:
+
+The person is at least 15 years old.
+They have parental supervision.
+The function accepts two parameters, age and isSupervised. Return a boolean. */
+
+function acceptIntoMovie(age, isSupervised) {
+	return age >= 15 || isSupervised === true;
+}
+
+// testing function 
+console.log(acceptIntoMovie(14, true)) // --> true
+console.log(acceptIntoMovie(15, true)) // --> true
+console.log(acceptIntoMovie(16, true)) // --> true
+console.log(acceptIntoMovie(14, false)) // --> false
+console.log(acceptIntoMovie(15, false)) // --> true
+console.log(acceptIntoMovie(16, false)) // --> true
+console.log(acceptIntoMovie(14.99999, true)) // --> true
+console.log(acceptIntoMovie(14.99999, false)) // --> false
