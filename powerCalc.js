e598957ba1d8e0f3cd193531ea301e5e84645670
karinapichardo <pichardokarina21@yahoo.com>
2020-10-21 11:02:31 -0400

    first commit

---
+++ powerCalc.js
@@ -0,0 +1,9 @@
+// Create a function that takes voltage and current and returns the calculated power.
+
+function circuitPower(voltage, current) {
+	return voltage * current;
+}
+
+console.log(circuitPower(230, 10)) // ➞ 2300
+console.log(circuitPower(110, 3)) // ➞ 330
+console.log(circuitPower(480, 20)) // ➞ 9600
