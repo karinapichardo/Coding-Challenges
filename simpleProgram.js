2c617254eaf02033ce11dba1be3ec4bfd14a26b5
karinapichardo <pichardokarina21@yahoo.com>
2020-10-29 13:19:48 -0400

    first commit

---
+++ simpleProgram.js
@@ -0,0 +1,16 @@
+/* A bartender is writing a simple program to determine whether he should
+serve drinks to someone. He only serves drinks to people 18 and older and
+when he's not on break.
+
+Given the person's age, and whether break time is in session, create a
+function which returns whether he should serve drinks. */
+
+function shouldServeDrinks(age, onBreak) {
+	return age >= 18 && onBreak === false;
+}
+
+console.log(shouldServeDrinks(17, true)) // false
+console.log(shouldServeDrinks(30, true)) // false
+console.log(shouldServeDrinks(24, false)) // true
+console.log(shouldServeDrinks(18, false)) // true
+console.log(shouldServeDrinks(3, false)) // false
